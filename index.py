d2bb5aa2e0f1aa4fec32b54d1266e5615deb70ca
Max Stoltenborgh <max.stoltenborgh@student.hu.nl>
2018-10-03 13:22:56 +0200

    Merge branch 'master' of C:\Users\Max Stoltenborgh\PycharmProjects\Project-Programmeren with conflicts.

--- index.py
+++ index.py
@@ -1,9 +1,11 @@
-import sqlite3
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE data
-             (bikeid INTEGER PRIMARY KEY ,name text, phonenumber text, securitycode text, checkedin INTEGER, time TEXT)''')
-c.execute("INSERT INTO data VALUES (83242, 'Henk Piet', '12345678', 'koe', 0, '00:00:00 01-01-1990')")
-c.execute("INSERT INTO data VALUES (43536, 'Rita Henksla', '87654321', 'eok', 0, '00:00:00 01-01-1990')")
-conn.commit()
-conn.close()
+import csv
+import os.path
+
+if os.path.exists('database.csv') == False:
+    with open('database.csv', 'w') as csvfile:
+        filewriter = csv.writer(csvfile, delimiter=',',
+                                quotechar='|', quoting=csv.QUOTE_MINIMAL)
+        filewriter.writerow(['bikeID', 'Name', 'Phonenumber', 'Securitycode', 'Registered', 'Time'])
+        print('Database has been created')
+else:
+    print('Database already exists')
