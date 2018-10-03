27d3cd88a9da5cca1b45a9a1f4138d66fb6eb229
xwolfyxNL <41stefan@gmail.com>
2018-10-03 12:16:42 +0200

    Added test data

--- index.py
+++ index.py
@@ -6,6 +6,10 @@ if os.path.exists('database.csv') == False:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
         filewriter.writerow(['bikeID', 'Name', 'Phonenumber', 'Securitycode', 'Registered', 'Time'])
+        # test data
+        filewriter.writerow(['14378427', 'Henk van de Piet', '12345678', 'koe', '', ''])
+        filewriter.writerow(['93247234', 'Jan van de Brug', '87654321', 'bruggetje', '', ''])
+        filewriter.writerow(['23022372', 'Janieta van de Dood', '82748234', 'makeup', '', ''])
         print('Database has been created')
 else:
     print('Database already exists')
