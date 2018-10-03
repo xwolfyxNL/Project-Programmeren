b5e37d6cd0499134d9932ea4a5ce5556a342c876
xwolfyxNL <41stefan@gmail.com>
2018-10-03 13:57:12 +0200

    Added if database exists statement

--- index.py
+++ index.py
@@ -1,9 +1,14 @@
 import sqlite3
-conn = sqlite3.connect('database.db')
-c = conn.cursor()
-c.execute('''CREATE TABLE data
-             (bikeid INTEGER PRIMARY KEY ,name text, phonenumber text, securitycode text, checkedin INTEGER, time TEXT)''')
-c.execute("INSERT INTO data VALUES (83242, 'Henk Piet', '12345678', 'koe', 0, '00:00:00 01-01-1990')")
-c.execute("INSERT INTO data VALUES (43536, 'Rita Henksla', '87654321', 'eok', 0, '00:00:00 01-01-1990')")
-conn.commit()
-conn.close()
+import os.path
+
+if os.path.exists('database.db') == False:
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE data
+                 (bikeid INTEGER PRIMARY KEY ,name text, phonenumber text, securitycode text, checkedin INTEGER, time TEXT)''')
+    c.execute("INSERT INTO data VALUES (83242, 'Henk Piet', '12345678', 'koe', 0, '00:00:00 01-01-1990')")
+    c.execute("INSERT INTO data VALUES (43536, 'Rita Henksla', '87654321', 'eok', 0, '00:00:00 01-01-1990')")
+    conn.commit()
+    conn.close()
+else:
+    print('Database exists')
