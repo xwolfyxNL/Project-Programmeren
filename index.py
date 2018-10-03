ad32ea081f722f8921251c023650c322ea3f4830
xwolfyxNL <41stefan@gmail.com>
2018-10-03 11:47:16 +0200

    Changed a column name to avoid confusion

--- index.py
+++ index.py
@@ -5,6 +5,6 @@ if os.path.exists('database.csv') == False:
     with open('database.csv', 'w') as csvfile:
         filewriter = csv.writer(csvfile, delimiter=',',
                                 quotechar='|', quoting=csv.QUOTE_MINIMAL)
-        filewriter.writerow(['ID', 'Name', 'Phonenumber', 'Securitycode', 'Registered', 'Time'])
+        filewriter.writerow(['ID', 'Name', 'Phonenumber', 'Securitycode', 'Checkin', 'Time'])
 else:
     print('okay')
