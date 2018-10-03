be6a358bb8c60ba47c8cf6d5f8c729661865f1ab
xwolfyxNL <41stefan@gmail.com>
2018-10-03 13:22:23 +0200

    Removed extra import statement

--- functions.py
+++ functions.py
@@ -14,7 +14,6 @@ def checkin(bikeID, name, phonenumber, Securitycode):
 def checkout():
 
 def fetchpersonalinfo(bikeid):
-    import sqlite3
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute("SELECT * from data where bikeid = ?", (bikeid,))
