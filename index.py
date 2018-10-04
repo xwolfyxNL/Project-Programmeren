19be40feb4690b6036e52976ac23f5289fb850f4
xwolfyxNL <41stefan@gmail.com>
2018-10-04 18:00:09 +0200

    Removed default database values

--- index.py
+++ index.py
@@ -10,8 +10,6 @@ if os.path.exists('database.db') == False:
     c = conn.cursor()
     c.execute('''CREATE TABLE data
                  (bikeid INTEGER PRIMARY KEY ,name text, phonenumber text, securitycode text, checkedin INTEGER, time TEXT)''')
-    c.execute("INSERT INTO data VALUES (83242, 'Henk Piet', '12345678', 'koe', 0, '00:00:00 01-01-1990')")
-    c.execute("INSERT INTO data VALUES (43536, 'Rita Henksla', '87654321', 'eok', 0, '00:00:00 01-01-1990')")
     conn.commit()
     conn.close()
 else:
